1aa72e1c58381aa72d25dc340b5a2b18364a398d
David Freitas <david@papaga.io>
2018-11-15 23:09:55 -0300

    Added Upcoming model

---
+++ ThemNewMovies/Model/Upcoming.swift
@@ -0,0 +1,27 @@
+//
+//  Upcoming.swift
+//  ThemNewMovies
+//
+//  Created by David Freitas on 15/11/18.
+//  Copyright © 2018 David Freitas. All rights reserved.
+//
+
+import Foundation
+
+struct Upcoming: Codable {
+    
+    let results: [Movie]
+    let totalResults: Int
+    let page: Int
+    let totalPages: Int
+    let dates: [String: Date]
+    
+
+    enum CodingKeys: String, CodingKey {
+        case results
+        case totalResults = "total_results"
+        case page
+        case totalPages = "total_pages"
+        case dates
+    }
+}
--- ThemNewMoviesTests/ModelTests.swift
+++ ThemNewMoviesTests/ModelTests.swift
@@ -46,5 +46,52 @@ class ModelTests: XCTestCase {
         // Test decoded data
         XCTAssertEqual(movie?.title, "The Seven Deadly Sins: Prisoners of the Sky")
     }
+    
+    // -----
+    // Test Upcoming decoding
+    // -----
+    func testUpcoming() {
+        let jsonString = """
+        {
+            "results": [
+                {
+                    "vote_count": 62,
+                    "id": 507569,
+                    "video": false,
+                    "vote_average": 5.6,
+                    "title": "The Seven Deadly Sins: Prisoners of the Sky",
+                    "popularity": 176.92,
+                    "poster_path": "/r6pPUVUKU5eIpYj4oEzidk5ZibB.jpg",
+                    "original_language": "en",
+                    "original_title": "劇場版 七つの大罪 天空の囚われ人",
+                    "genre_ids": [ 28, 12, 14, 16],
+                    "backdrop_path": "/uKwOX7MtKlAaGeCQe6c4jc1vZpj.jpg",
+                    "adult": false,
+                    "overview": "Traveling ...",
+                    "release_date": "2018-08-18"
+                }
+            ],
+            "page": 1,
+            "total_results": 262,
+            "dates": {
+                "maximum": "2018-12-11",
+                "minimum": "2018-11-22"
+            },
+            "total_pages": 14
+        }
+        """
+        
+        guard let data = jsonString.data(using: String.Encoding.utf8) else {
+            fatalError()
+        }
+        
+        let upcoming: Upcoming? = ModelDecoder.decodeJSON(withData: data)
+        
+        // Test decoding success
+        XCTAssertNotNil(upcoming)
+        
+        // Test decoded data
+        XCTAssertEqual(upcoming?.totalPages, 14)
+    }
 
 }
